291014837f43a7cd9360cf5ced46240f11515c57
rohit <rohitkvartak4@gmail.com>
2023-06-14 23:14:21 +0530

    rnn model updated

--- src/models/rnn_1hl.py
+++ src/models/rnn_1hl.py
@@ -12,12 +12,12 @@ class RNN:
 
 
 
-    def _intialize(self):
+    def initialize(self):
         """Intialize the weights and biases of the RNN."""
         np.random.seed(self.seed)
-        self.Wxh = np.random.randn(self.input_nodes, self.hidden_nodes) * 0.01 #the weights for the input layer
-        self.Whh = np.random.randn(self.hidden_nodes, self.hidden_nodes) * 0.01 #the weights for the hidden layers
-        self.Why = np.random.randn(self.hidden_nodes, self.output_nodes) * 0.01 #the weights for the output layer
+        self.Wxh = np.random.randn(self.input_nodes, self.hidden_nodes) #the weights for the input layer
+        self.Whh = np.random.randn(self.hidden_nodes, self.hidden_nodes) #the weights for the hidden layers
+        self.Why = np.random.randn(self.hidden_nodes, self.output_nodes) #the weights for the output layer
         self.bh = np.zeros((1, self.hidden_nodes)) #the bias for the hidden layers
         self.by = np.zeros((1, self.output_nodes)) #the bias for the output layer
     
@@ -33,13 +33,13 @@ class RNN:
         #perform the forward propagation step through the RNN
         self.h = np.zeros((X.shape[0], self.hidden_nodes)) #the hidden state
         self.h_prev = np.zeros((X.shape[0], self.hidden_nodes)) #the previous hidden state
-        self.y = np.zeros((X.shape[0], self.output_nodes)) #the output state
+        self.y = np.zeros((X.shape[0], X.shape[1], self.output_nodes)) #the output state
         self.p = np.zeros((X.shape[0], self.output_nodes)) #the probability distribution of the output state
         self.X = X #the input data
         for t in range(X.shape[1]):
             self.h_prev = self.h
             self.h = np.tanh(np.dot(X[:,t,:], self.Wxh) + np.dot(self.h_prev, self.Whh) + self.bh)
-            self.y = np.dot(self.h, self.Why) + self.by
-            self.p = utils.softmax(self.y)
-        return self.p, self.h
+            self.y[:, t, :] = np.dot(self.h, self.Why) + self.by #update the output state
+            #self.p = utils.softmax(self.y)
+        return self.y
 
