9a5f8f7105f6ebd7f4b5fa4cfc01fdc6850271e8
rohit <rohitkvartak4@gmail.com>
2023-06-14 18:44:31 +0530

    config added

---
+++ src/config.py
@@ -0,0 +1,12 @@
+import yaml
+
+global cfg
+if 'cfg' not in globals():
+    with open('src/config.yml', 'r') as f:
+        cfg = yaml.load(f, Loader=yaml.FullLoader)
+
+
+def get_cfg():
+    """Return the global config object."""
+    return cfg
+
