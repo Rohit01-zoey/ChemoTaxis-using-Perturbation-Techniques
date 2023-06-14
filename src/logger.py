af90dcd8994b5e0d998c3e7425995ec87773356d
rohit <rohitkvartak4@gmail.com>
2023-06-14 23:12:48 +0530

    logger added

--- src/logger.py
+++ src/logger.py
@@ -1,32 +1,67 @@
 import logging
+import os
+import time
 
-def setup_logger(log_file):
+class Logger:
+    """Logger class helper to log training progress to a file and the console.
     """
-    Set up a logger with a file handler.
+    def __init__(self, log_file, experiment_title):
+        self.log_file = log_file
+        self.experiment_title = experiment_title
+        self.logger = self._setup_logger()
 
-    Parameters:
-    -----------
-    log_file : str
-        Path to the log file.
+    def _setup_logger(self):
+        # Create the log directory if it doesn't exist
+        log_dir = os.path.dirname(self.log_file)
+        os.makedirs(log_dir, exist_ok=True)
 
-    Returns:
-    --------
-    logger : logging.Logger
-        Logger object.
+        # Set up logging configuration
+        logging.basicConfig(level=logging.INFO,
+                            format='%(asctime)s [%(levelname)s] %(message)s',
+                            handlers=[
+                                logging.FileHandler(self.log_file),
+                                logging.StreamHandler()
+                            ])
 
-    """
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
+        # Add experiment title to the log file
+        logging.getLogger().info('Experiment: {}'.format(self.experiment_title))
 
-    # Create file handler and set its level to DEBUG
-    file_handler = logging.FileHandler(log_file)
-    file_handler.setLevel(logging.DEBUG)
+        # Return the logger object
+        return logging.getLogger()
 
-    # Create formatter and add it to the file handler
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    file_handler.setFormatter(formatter)
+    def log_epoch(self, epoch, train_loss, val_loss):
+        # Log epoch information
+        self.logger.info("Epoch: {} | Train Loss: {:.4f} | Val Loss: {:.4f}".format(epoch, train_loss, val_loss))
 
-    # Add the file handler to the logger
-    logger.addHandler(file_handler)
+    def log_time(self, epoch, elapsed_time):
+        # Log time information
+        self.logger.info("Time elapsed for epoch {}: {:.2f} seconds".format(epoch, elapsed_time))
 
-    return logger
+    def close(self):
+        # Close the logger to release any resources
+        logging.shutdown()
+
+# Usage example
+# if __name__ == '__main__':
+#     # Set up logger
+#     log_file = 'logs/my_project.log'
+#     experiment_title = 'Experiment 1'
+#     logger = Logger(log_file, experiment_title)
+
+#     # Log training progress
+#     for epoch in range(1, 6):
+#         start_time = time.time()
+
+#         # Training code...
+#         train_loss = 0.123
+#         val_loss = 0.045
+
+#         # Log epoch information
+#         logger.log_epoch(epoch, train_loss, val_loss)
+
+#         # Log time information
+#         elapsed_time = time.time() - start_time
+#         logger.log_time(epoch, elapsed_time)
+
+#     # Close the logger to release any resources
+#     logger.close()
