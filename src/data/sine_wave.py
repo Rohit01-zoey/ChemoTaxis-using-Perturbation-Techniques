4568c763f6ac04898b52cfcf73b0bf1c7af45145
rohit <rohitkvartak4@gmail.com>
2023-06-15 10:23:01 +0530

    sine augmentation added

--- src/data/sine_wave.py
+++ src/data/sine_wave.py
@@ -2,7 +2,8 @@ import numpy as np
 
 
 class SineWaveLoader:
-    def __init__(self, time_stamps, amplitude=1.0, frequency=1.0, phase=0.0):
+    def __init__(self, n_samples, time_stamps, amplitude=1.0, frequency=1.0, phase=0.0):
+        self.n_samples = n_samples
         self.time_stamps = time_stamps
         self.amplitude = amplitude
         self.frequency = frequency
@@ -12,6 +13,15 @@ class SineWaveLoader:
         timestamps = np.linspace(0, 1, self.time_stamps)
         sine_wave = self.amplitude * np.sin(2 * np.pi * self.frequency * timestamps + self.phase)
         sine_wave = sine_wave.reshape((1, self.time_stamps, 1))
+        sine_wave_dict = {'train' : sine_wave, 'val' : sine_wave, 'test' : sine_wave}
+        return sine_wave_dict
+    
+    def add_noise(self, noise_level):
+        sine_wave = self.load_data()['train']
+        new_shape = sine_wave.shape
+        new_shape = (self.n_samples, new_shape[1], new_shape[2])
+        noise = np.random.normal(0, noise_level, new_shape)
+        sine_wave = sine_wave + noise
         sine_wave_dict = {'train' : sine_wave, 'val' : sine_wave, 'test' : sine_wave}
         return sine_wave_dict
 
