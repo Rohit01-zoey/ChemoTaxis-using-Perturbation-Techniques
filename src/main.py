a350a816d3384af7cad1e4ff85265dfff34fac25
rohit <rohitkvartak4@gmail.com>
2023-06-19 19:25:00 +0530

    updated

--- src/main.py
+++ src/main.py
@@ -10,9 +10,9 @@ from optimizer import adam, rmsprop
 from modules import fsm
 
 
-sine = sine_wave.SineWaveLoader(60, 100, amplitude=10.0, frequency=1.0, phase=0.0)
+sine = sine_wave.SineWaveLoader(50, 100, amplitude=10.0, frequency=10.0)
 sine_wave_dict = sine.load_data()
-sine_wave_dict = sine.add_noise(1)
+# sine_wave_dict = sine.add_noise(1)
 # plt.plot(sine_wave_dict['train'][1, :, 0])
 # plt.show()
 
@@ -28,20 +28,23 @@ log_file = logger.Logger(cfg['log']['log_file'], cfg['log']['experiment_name'])
 rnn_model = rnn_1hl.RNN(input_size, hidden_size, output_size, seed=seed)
 rnn_model.initialize()
 
-network.train(cfg, rnn_model, sine.load_data(), logger=log_file)
+network.train(cfg, rnn_model, sine_wave_dict, logger=log_file)
 
 log_file.close() #free up the logger file
 
 plt.figure()
-plt.plot(sine.load_data()['val'][0, :, 0], 'r', label = "True")
-plt.plot(rnn_model.forward(sine.load_data()['val'])[0, :, 0], 'b', label = "Predicted")
+plt.plot(sine_wave_dict['test'][0, :, 0], 'r', label = "True")
+fsm_model = fsm.FSM(rnn_model)
+generated_sequence = fsm_model(sine_wave_dict['test'][:, 0:1, :], time = 100)
+#plt.plot(rnn_model.forward(sine_wave_dict['val'])[0, :, 0], 'b', label = "Predicted")
+plt.plot([generated_sequence[0, i, 0] for i in range(fsm_model.time)], 'b', label = "Predicted")
 plt.legend()
 plt.show()
 
 
-fsm_model = fsm.FSM(rnn_model)
-fsm_model(np.random.rand(1, 1, 1), time = 100)
+# fsm_model = fsm.FSM(rnn_model)
+# fsm_model(np.random.rand(1, 1, 1), time = 100)
 
-plt.figure()
-plt.plot([fsm_model.generated_sequence[0, i, 0] for i in range(fsm_model.time)], 'r', label = "gen")
-plt.show()
+# plt.figure()
+# plt.plot([fsm_model.generated_sequence[0, i, 0] for i in range(fsm_model.time)], 'r', label = "gen")
+# plt.show()
--- src/modules/fsm.py
+++ src/modules/fsm.py
@@ -22,18 +22,18 @@ class FSM:
                 X.shape[1]==1 #the number of input data is only 1 time step since RNNs assume current sample depends on previous sample
             except:
                 raise Exception('The input data must be 3D of the shape ({}, 1, {}). The shape of the input data is: {}'.format(X.shape[0], X.shape[2], X.shape))
-            generated_sequence = np.zeros((X.shape[0], self.time+1, X.shape[2]))
-            generated_sequence[:, 0, :] = X #initialize the first time step of the generated sequence to be the first time step of the input data
+            generated_sequence = np.zeros((X.shape[0], self.time, X.shape[2]))
+            generated_sequence[:, 0, :] = X[:, 0, :] #initialize the first time step of the generated sequence to be the first time step of the input data
 
             #perform the forward propagation step through the RNN
             h = np.zeros((X.shape[0], self.model.hidden_nodes)) #the hidden state
             h_prev = np.zeros((X.shape[0], self.model.hidden_nodes)) #the previous hidden state
-            for t in range(1, self.time+1):
+            for t in range(1, self.time):
                 h_prev = h
-                h = np.tanh(np.dot(generated_sequence[:, t-1:t, :], self.model.Wxh) + np.dot(h_prev, self.model.Whh) + self.model.bh)
+                h = np.tanh(np.dot(generated_sequence[:, -1, :], self.model.Wxh) + np.dot(h_prev, self.model.Whh) + self.model.bh)
                 generated_sequence[:, t, :] = np.dot(h, self.model.Why) + self.model.by #update the output state
             
-            self.generated_sequence = generated_sequence
+            return generated_sequence
         
         elif self.state == 'idle':
             print('The FSM is in the idle state.')
