fa08a96960cfee27c232bc1a8029a414079db877
rohit <rohitkvartak4@gmail.com>
2023-06-19 19:24:48 +0530

    sine data added

--- src/data/sine_wave.py
+++ src/data/sine_wave.py
@@ -9,26 +9,35 @@ import numpy as np
 
 class SineWaveLoader:
     """Sine wave loader class loads a single sine wave with a given amplitude, frequency and phase."""
-    def __init__(self, time_stamps, amplitude, phase, frequency=1.0):
+    
+    SEED = 42
+    
+    def __init__(self, n_samples, time_stamps, amplitude, frequency=1.0):
         if isinstance(amplitude, (int, float)) and isinstance(frequency, (int, float)):
             # ensure that we input the same amplitudes and frequencies for every phase
+            self.n_samples = n_samples # number of samples
             self.time_stamps = time_stamps # number of time stamps
             self.amplitude = amplitude # array of amplitudes
             self.frequency = frequency # array of frequencies
-            self.phase = phase # array of phases
+            self.phase = np.random.rand(self.n_samples) * 2 * np.pi # array of phases
         else:
             raise ValueError("Amplitude and frequency should be float values.")
 
     def load_data(self):
-        sind_data = np.zeros((len(self.phase), self.time_stamps, 1))
+        sine_data = np.zeros((self.n_samples, self.time_stamps, 1))
         index = 0
         for phase in self.phase:
             timestamps = np.linspace(0, 1, self.time_stamps)
             sine_wave = self.amplitude * np.sin(2 * np.pi * self.frequency * timestamps + phase)
             sine_wave = sine_wave.reshape((1, self.time_stamps, 1))
-            sind_data[index] = sine_wave
+            sine_data[index] = sine_wave
             index += 1 # updating the index to append to data array
-        return sind_data
+        random_perm = np.random.permutation(self.n_samples)
+        train_data = sine_data[random_perm[:int(0.6 * self.n_samples)], :, :]
+        val_data = sine_data[random_perm[int(0.6 * self.n_samples) : int(0.8 * self.n_samples)], :, :]
+        test_data = sine_data[random_perm[int(0.8 * self.n_samples) :], :, :]
+        sine_data_dict = {'train' : train_data, 'val' : val_data, 'test' : test_data}
+        return sine_data_dict
     
     def add_noise(self, noise_level):
         sine_wave = self.load_data()['train']
@@ -41,8 +50,8 @@ class SineWaveLoader:
 
 
 
-sine_class = SineWaveLoader(100, amplitude= [10, 12], frequency=5, phase=[0.0, 10, 20, 30])
-data = sine_class.load_data()
-import matplotlib.pyplot as plt
-plt.plot(data[1, :, 0])
-plt.show()
+# sine_class = SineWaveLoader(60, 100, amplitude=10.0, frequency=10.0)
+# data = sine_class.load_data()
+# import matplotlib.pyplot as plt
+# plt.plot(data['train'][1, :, 0])
+# plt.show()
